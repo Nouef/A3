4593076f368f221774a3cd0c71153a3fca92e405
Nouef <161381799+Nouef@users.noreply.github.com>
2024-04-19 23:49:41 +0400

    Update code.py

--- code.py
+++ code.py
@@ -15,4 +15,13 @@ class PostManager:
 
     def get_post_by_datetime(self, datetime):
         return self.post_table.get(datetime)
+
+# Test cases for Part 1
+post_manager = PostManager()
+post1 = Post("2024-04-18 10:00:00", "Hello World!", "User1", 100)
+post2 = Post("2024-04-18 11:00:00", "Good morning!", "User2", 50)
+post_manager.add_post(post1)
+post_manager.add_post(post2)
+print(post_manager.get_post_by_datetime("2024-04-18 10:00:00").content)  
+print(post_manager.get_post_by_datetime("2024-04-18 11:00:00").author)
   
